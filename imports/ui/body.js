136662e12d39c2a0117908a5f43ff9e0119b1a2f
Adam Cornforth <adam@mojofuel.com>
2016-04-01 14:30:56 +0100

    Add 'hide completed tasks' checkbox using ReactiveDict to store UI state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,32 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+// Set up a new ReactiveDict and attach it to the body
+// template instance (where we'll store the checkbox's state)
+Template.body.onCreated(function bodyOnCreated() {
+	// Good for storing the UI state as ReactiveDict does
+	// not sync with the server like collections do 
+	this.state = new ReactiveDict(); 
+});
+
 // Helpers are used to pass data into templates
 // The below code defines a helper called tasks that returns an array
 Template.body.helpers({
 	tasks() {
-		return Tasks.find({}, { sort: { createdAt: -1} });
+		const instance = Template.instance(); 
+		if(instance.state.get('hideCompleted')) {
+			// If hide completed is checked, filter tasks 
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1} });
+		}
+		return Tasks.find({}, { sort: { createdAt: -1 } });
+	},
+	incompleteCount() {
+		return Tasks.find({ checked: { $ne: true } }).count();
 	}
 });
 
@@ -35,5 +52,8 @@ Template.body.events({
 
 		// Clear form 
 		target.text.value = '';
+	},
+	'change .hide-completed input'(event, instance) { 
+		instance.state.set('hideCompleted', event.target.checked);
 	},
 });
