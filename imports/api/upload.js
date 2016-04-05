996480ac7abdc43e9c41c053f3e8f2802dd91b34
Adam Cornforth <adam@apcdesigner.com>
2016-04-05 20:20:46 +0100

    Clean up uploads route

--- imports/api/upload.js
+++ imports/api/upload.js
@@ -6,44 +6,32 @@ import { Uploads } from '../api/images.js';
 var image_id; 
 var files;
 
-FS.HTTP.setBaseUrl('/files');
-
 Router.route('/upload', { where: 'server' })
-  .get(function () {
-    console.log('get upload');
-  	var res = this.response;
-  	res.end('Hello\n');
-  })
   .post(function (data) {
-    // console.log('post upload');
   	files = this.request.files;
-    // console.log('files', files);
     var res = this.response;
-    // console.log('res');
-    if (true)
-      // console.log('test');
-      // Insert into the tasks collection
-      Meteor.call('tasks.pi_insert', "Pi", function(err, _id) {
-        // console.log(err);
-        image_id = _id;
-        var file = new FS.File();
-      
-        file.attachData(files[0].data, {type: files[0].mimeType},function(err){
-            // console.log(err, Uploads);
-            file.name(files[0].filename);
 
-            Uploads.insert(file, function (err, fileObj) {
-              // console.log(err);
-              while(fileObj.url()==null);
+    // Insert into the tasks collection
+    Meteor.call('tasks.pi_insert', "Pi", function(err, _id) {
 
-              var resp = {
-                'url': fileObj.S3Url("images"),
-              };
-              Meteor.call('tasks.setUrl', image_id, resp.url);
-              res.end(JSON.stringify(resp));
-            });
-        });
+      image_id = _id;
+      var file = new FS.File();
+    
+      file.attachData(files[0].data, {type: files[0].mimeType},function(err){
+          file.name(files[0].filename);
+
+          Uploads.insert(file, function (err, fileObj) {
+            while(fileObj.url()==null);
+
+            var resp = {
+              'url': fileObj.S3Url("images"),
+            };
+            Meteor.call('tasks.setUrl', image_id, resp.url);
+            res.end(JSON.stringify(resp));
+          });
       });
 
-  	res.end('Uploading..?\n');
+  	res.end('File uploaded\n');
   });
+
+});
